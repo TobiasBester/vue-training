877f1895424602ca7edf40a8567c7b155cb9f241
Tobias <tobias@labs.epiuse.com>
2022-01-20 16:58:58 +0200

    Add BaseBtn component
    - Add BaseBtn component to hide away ugly
    Tailwind classes

---
+++ vue-training-app/src/components/base/BaseBtn.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    class="group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white"
+    :class="`${colorClasses} ${focus ? 'focus:outline-none focus:ring-2 focus:ring-offset-2' : ''}`"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue';
+
+  const props = defineProps({
+    color: {
+      type: String,
+      default: 'red'
+    },
+    focus: {
+      type: Boolean,
+      default: false
+    }
+  })
+
+  const colorClasses = computed(() => {
+    switch (props.color) {
+      case 'red':
+        return 'bg-red-600 hover:bg-red-700 focus:ring-red-500'
+      case 'green':
+        return 'bg-green-600 hover:bg-green-700 focus:ring-green-500'
+      default:
+        return 'bg-amber-600 hover:bg-amber-700 focus:ring-amber-500'
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
